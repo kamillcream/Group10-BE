da4020f5e4a2b1cd165905ef299ef035e77ae35b
박진영 <am030813@naver.com>
2024-12-23 17:36:09 +0700

    FIX: DistanceStandard 컴포넌트 등록

--- src/main/java/chiangmai/enumeration/DistanceStandard.java
+++ src/main/java/chiangmai/enumeration/DistanceStandard.java
@@ -2,9 +2,11 @@ package chiangmai.enumeration;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @Getter
+@Component
 public enum DistanceStandard{
 
     HALF(300),
