e0174e06f2edfe606b3ec898639f2d86246dd997
박진영 <am030813@naver.com>
2024-12-23 19:51:10 +0700

    FEAT: 랭킹 데이터에 credit 포함

---
+++ src/main/java/chiangmai/domain/User.java
@@ -0,0 +1,31 @@
+package chiangmai.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "user_id")
+    private String userId;
+    private String password;
+    @Column(name = "`name`")
+    private String name;
+    @Column(name = "`rank`")
+    private Integer rank;
+    private Long credit;
+    private double total; // 총 도보량
+    private double startX;
+    private double startY;
+    private double endX;
+    private double endY;
+    private double currentX;
+    private double currentY;
+}
